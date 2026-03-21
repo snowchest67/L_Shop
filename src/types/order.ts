1afe239e982c4e16491f01c31008c30226dc1a5f
snowchest67 <snowchest67@tut.by>
2026-03-21 21:26:15 +0300

    add cart.ts and order.ts

---
+++ src/types/order.ts
@@ -0,0 +1,24 @@
+export type PaymentMethod = 'card' | 'cash' | 'online'
+
+export interface DeliveryInfo {
+	address: string
+	phone: string
+	email: string
+	paymentMethod: PaymentMethod
+}
+
+export interface OrderItem {
+	productId: string
+	quantity: number
+	unitPrice: number
+}
+
+export interface Order {
+	id: string
+	userId: string
+	items: OrderItem[]
+	deliveryInfo: DeliveryInfo
+	totalPrice: number
+	status: 'pending' | 'completed'
+	createdAt: string
+}
